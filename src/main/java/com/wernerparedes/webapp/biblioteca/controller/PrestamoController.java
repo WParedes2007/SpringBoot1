6618ca3031558a623ed4b24c141f4617dfdf86e8
mchuquiej-2021101 <mchuquiej-2021101@kinal.edu.gt>
2024-08-09 16:13:35 -0600

    Validacion Terminada

--- src/main/java/com/wernerparedes/webapp/biblioteca/controller/PrestamoController.java
+++ src/main/java/com/wernerparedes/webapp/biblioteca/controller/PrestamoController.java
@@ -59,7 +59,7 @@ public class PrestamoController {
         }
     }
 
-    @PutMapping("/libro")
+    @PutMapping("/prestamo")
     public ResponseEntity<Map<String, String>>editarPrestamo(@RequestParam Long id, @RequestBody Prestamo prestamoNuevo){
         Map<String,String> response = new HashMap<>();
         try {
--- src/main/java/com/wernerparedes/webapp/biblioteca/model/ManuToOne.java
+++
@@ -1,5 +0,0 @@
-package com.wernerparedes.webapp.biblioteca.model;
-
-public @interface ManuToOne {
-
-}
